0b49aae3692825d8f4f4204185555839b1630714
Pierre Barbé <pierrebarbe0@gmail.com>
2023-10-07 17:53:30 +0200

    wip header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				'09494C': '#09494C',
+			},
+			colors: {
+				'09494C': '#09494C',
+				'EFE6D3': '#EFE6D3'
+			},
+		},
 	},
 	plugins: [],
 }
