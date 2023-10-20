8c39c843f706ff79de185e609661c347905ed3ba
maxentr <rouaultmaxent@gmail.com>
2023-10-20 13:48:48 +0200

    style: finish header responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				'xs': '475px',
+			},
 			backgroundColor: {
 				'09494C': '#09494C',
 				'EFE6D3': '#EFE6D3',
