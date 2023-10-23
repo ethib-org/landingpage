9b77250b2fb6e34b0935fa8f12e87e2513b393e6
maxentr <rouaultmaxent@gmail.com>
2023-10-23 10:35:36 +0200

    build: add robot.txt and compress files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://ethib.fr',
+  integrations: [tailwind(), compress(), robotsTxt({
+    host: true,
+    sitemap: false,
+  })]
 });
