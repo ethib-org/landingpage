d3e018cf81e23fcbec8b7fe61746e1bb92059fdb
maxentr <rouaultmaxent@gmail.com>
2023-11-01 00:49:16 +0100

    build: add astro-compressor and move astro itegrations to devDeps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import robotsTxt from 'astro-robots-txt';
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ethib.fr',
   integrations: [tailwind(), compress(), robotsTxt({
     host: true,
-    sitemap: false,
-  })]
+    sitemap: false
+  }), compressor()]
 });
